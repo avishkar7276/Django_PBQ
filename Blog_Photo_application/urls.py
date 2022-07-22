715eec2fafd59a0f2fd9c2320f9ccf4aa1c8d270
avishkarPansare <avishkarpansare88@gmail.com>
2022-07-22 18:28:25 +0530

    urlfile changes

--- Blog_Photo_application/urls.py
+++ Blog_Photo_application/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('', include('Blog_Photo_App.urls')),
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-from django.conf.urls import url
-# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.views.static import serve
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# from django.conf.urls import url
+# # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.views.static import serve
 
-# Serve static and media files from development server
-# urlpatterns += staticfiles_urlpatterns()
-urlpatterns += url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-urlpatterns += url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+# # Serve static and media files from development server
+# # urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+# urlpatterns += url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
